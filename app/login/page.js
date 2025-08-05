8919d83470e035d4a07f4a93f1aea15f2340692b
AMITH E K <amithajith7025@gmail.com>
2025-08-05 17:35:46 +0530

    updated the error

--- app/login/page.js
+++ app/login/page.js
@@ -10,15 +10,15 @@ const page = () => {
                 </div>
                 <div>
                     <form method='POST' className='flex flex-col gap-2 mb-4 px-4 py-2'>
-                        <label htmlFor='username' className='text-sm font-medium text-gray-700b cursor-pointer'>Username</label>
-                        <input type='text' id='username' className='border-2 border-gray-400 rounded-md px-4 py-2 focus:outline-non focus:ring-2 focus:ring-purple-500 transition duration-200' />
+                        <label htmlFor='username' className='text-sm font-medium text-gray-700 cursor-pointer'>Username</label>
+                        <input type='text' id='username' className='border-2 border-gray-400 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-purple-500 transition duration-200' />
                         <label htmlFor='password' className='text-sm font-medium text-gray-700b cursor-pointer'>Password</label>
-                        <input type='password' id='password' className='border-2 border-gray-400 rounded-md px-4 py-2 focus:outline-non focus:ring-2 focus:ring-purple-500 transition duration-200 mb-2' />
+                        <input type='password' id='password' className='border-2 border-gray-400 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-purple-500 transition duration-200 mb-2' />
                         <button type='submit' className='bg-indigo-700 h-9 rounded-md text-white font-semibold font-mono hover:bg-indigo-800 transition duration-200 cursor-pointer'>Login</button>
                     </form>
                 </div>
                 <div className='flex justify-center'>
-                    <p>Don't have an account? <Link href="/register"><span className='cursor-pointer text-indigo-700'>register</span></Link></p>
+                    <p>Don&apos;t have an account?<Link href="/register"><span className='cursor-pointer text-indigo-700'>register</span></Link></p>
                 </div>
             </div>
         </div>
