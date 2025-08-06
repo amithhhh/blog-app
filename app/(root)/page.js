2cdd8e33d694c6c282e87236b32995066eafd556
AMITH E K <amithajith7025@gmail.com>
2025-08-06 20:23:22 +0530

    rectified the ui associated in root page

--- app/(root)/page.js
+++ app/(root)/page.js
@@ -4,11 +4,11 @@ import BlogBox from "@/components/BlogBox";
 
 export default function Home() {
   return (
-    <div className="font-sans min-h-screen p-8 pb-20 gap-16 sm:p-20 lg:items-center lg:justify-items-center shadow-lg">
+    <div className="font-sans min-h-screen p-4 pb-16 gap-16 sm:p-20 lg:items-center lg:justify-items-center shadow-lg items-center">
       <h2 className="font-mono font-bold text-4xl text-gray-700 mb-7">Blog Posts</h2>
       <form method="GET" className="mb-5 flex flex-row">
-        <input type="text" className='border-2 border-gray-400 rounded-md px-2 py-1 focus:outline-none focus:ring-2 focus:ring-purple-500 transition duration-200' name="query" />
-        <button type="submit" className="ml-4 bg-indigo-700 px-8 py-2 rounded-lg text-white font-mono font-sm cursor-pointer hover:bg-indigo-800" onClick={(e) => e.preventDefault()}>Search</button>
+        <input type="text" className='border-2 border-gray-400 rounded-md px-2 py-1 focus:outline-none focus:ring-2 focus:ring-purple-500 transition duration-200 w-50' name="query" />
+        <button type="submit" className="ml-4 bg-indigo-700 px-6 py-2 rounded-lg text-white font-mono font-sm cursor-pointer hover:bg-indigo-800" onClick={(e) => e.preventDefault()}>Search</button>
       </form>
       <div className="flex flex-row flex-wrap gap-9">
         {
