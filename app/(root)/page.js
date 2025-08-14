40d46264a8ab37030a1c472d81db524cef8179e4
AMITH E K <amithajith7025@gmail.com>
2025-08-14 06:16:08 +0530

    designed profile page

--- app/(root)/page.js
+++ app/(root)/page.js
@@ -4,7 +4,7 @@ import BlogBox from "@/components/BlogBox";
 
 export default function Home() {
   return (
-    <div className="font-sans min-h-screen p-4 pb-16 gap-16 sm:p-20 lg:items-center lg:justify-items-center shadow-lg items-center">
+    <div className="font-mono min-h-screen p-4 pb-16 gap-16 sm:p-20 lg:items-center lg:justify-items-center shadow-lg items-center">
       <h2 className="font-mono font-bold text-4xl text-gray-700 mb-7">Blog Posts</h2>
       <form method="GET" className="mb-5 flex flex-row">
         <input type="text" className='border-2 border-gray-400 rounded-md px-2 py-1 focus:outline-none focus:ring-2 focus:ring-purple-500 transition duration-200 w-50' name="query" />
--- app/(root)/profile/[id]/page.js
+++ app/(root)/profile/[id]/page.js
@@ -1,15 +1,40 @@
 'use client';
 
-import { useParams } from 'next/navigation'
-import React from 'react'
+import { useParams } from 'next/navigation';
+import React from 'react';
+import Image from 'next/image';
 
 function ProfilePage() {
-    const { id } = useParams();
+  const { id } = useParams();
+
   return (
-    <div>
-      {id}
+    <div className='flex justify-center bg-gray-50 p-20'>
+      <div className='w-full max-w-md lg:shadow-lg rounded-lg flex flex-col p-4 items-center gap-6 bg-white'>
+        <Image
+          src='/istockphoto-2171382633-170667a.jpg'
+          alt='Profile picture'
+          width={100}
+          height={100}
+          className='rounded-full'
+        />
+
+        <h2 className='text-2xl font-mono font-bold text-indigo-700'>Adolf Hitler</h2>
+
+        <div className='flex flex-col lg:flex-row items-center justify-center gap-10'>
+          <div className='w-50 h-50 bg-indigo-700 rounded-lg shadow-xl text-white'>
+            <h2 className='text-4xl font-bold font-sans '>50</h2>
+            <p className='text-2xl font-mono'>No of Blogs</p>
+          </div>
+          <div className='w-20 h-20 bg-indigo-700 flex items-center justify-center rounded-lg shadow-xl text-white'>
+            2
+          </div>
+          <div className='w-20 h-20 bg-indigo-700 flex items-center justify-center rounded-lg shadow-xl text-white'>
+            3
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ProfilePage
+export default ProfilePage;
